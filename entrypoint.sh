0518b0ae39a90b0681e08fa615ce3cc3613bb5d3
weviesAdmin <143383994+weviesAdmin@users.noreply.github.com>
2024-08-14 03:03:49 +0900

    Update entrypoint.sh
    yarn to npm

--- entrypoint.sh
+++ entrypoint.sh
@@ -32,11 +32,12 @@ aws_access_key_id = ${AWS_ACCESS_KEY_ID}
 aws_secret_access_key = ${AWS_SECRET_ACCESS_KEY}" > ~/.aws/credentials
 
 echo "Install dependencies"
-yarn install
+# yarn install
 npm ci
 
 echo "Run yarn build"
-yarn run build
+# yarn run build
+npm run build
 
 echo "Copying to website folder"
 aws s3 sync ./build s3://${AWS_S3_BUCKET} --exact-timestamps --delete --region ${AWS_DEFAULT_REGION} $*
